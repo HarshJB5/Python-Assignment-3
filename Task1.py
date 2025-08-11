da912760eebaeb69e699947101fd49ddf3d0ac06
HarshJB5 <harsadeepta2025@gmail.com>
2025-08-12 01:36:55 +0530

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,10 @@
+a=int(input("Enter a number:"))
+
+def factorial(a):
+    if a<=1:
+        return 1
+    else:
+        return a*factorial(a-1)
+
+result=factorial(a)
+print("factorial of",a,"is",result)
---
+++ Task2.py
@@ -0,0 +1,6 @@
+a=int(input("enter a number"))
+import math
+print("Square root of",a,"is",math.sqrt(a))
+print("logarithmic base",a,"is",math.log(a))
+print("sin base",a,"is",math.sin(a))
+print("Thank You")
